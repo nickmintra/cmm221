b44ccec64c63bd407bb26581522a71370e2bdc5e
nickmintra <32549968+nickmintra@users.noreply.github.com>
2017-10-07 16:13:31 +0700

    Add content to twig

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,3 +1,32 @@
 exports.index = function(req, res) {
-	res.render('home.twig')
+	let content = {
+		highlight: {
+			url:'/assets/images/home/banner/highlight.jpg',
+			title:'Broccoli garden\'s uncle Ken'
+		},
+		thumbnail:[
+			{
+				url: '/assets/images/home/banner/thumbnail-1.jpg',
+				title: ' Product from my garden'
+			},
+			{
+				url: '/assets/images/home/banner/thumbnail-2.jpg',
+				title: 'Uncle Ken Hotel '
+			},
+			{
+				url: '/assets/images/home/banner/thumbnail-3.jpg',
+				title: 'Uncle Ken café'
+			},
+			{
+				url: '/assets/images/home/banner/thumbnail-4.jpg',
+				title: 'Relex Programm'
+			},
+			{
+				url: '/assets/images/home/banner/thumbnail-5.jpg',
+				title: 'Adventure Programm'
+			}
+		]
+	}
+
+	res.render('home.twig' , content)
 }
--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -1,18 +1,3 @@
 exports.index = function(req, res) {
-	let content = {
-		news: [
-		{
-			id: 1,
-			url: '//image/img-1.jpg',
-			title: 'news1'
-		},
-		{
-			id: 2,
-			url: '//image/img-1.jpg',
-			title: 'news2'
-		}
-	]
-}
-
-	res.json(content)
+	res.render('news.twig')
 }
